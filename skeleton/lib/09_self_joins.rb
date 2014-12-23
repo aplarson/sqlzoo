0127c14df76e9f190fe68d5cad72628410d98598
davidrunger <daverunger@gmail.com>
2014-12-22 16:04:51 -0800

    provide missing example query

--- skeleton/lib/09_self_joins.rb
+++ skeleton/lib/09_self_joins.rb
@@ -33,7 +33,18 @@ def lrt_stops
 end
 
 def connecting_routes
-  # The query shown gives the number of routes that visit either London Road
+  # Consider the following query:
+  #
+  # SELECT
+  #   company, num, COUNT(*)
+  # FROM
+  #   route
+  # WHERE
+  #   stop=149 OR stop=53
+  # GROUP BY
+  #   company, num
+  #
+  # The query gives the number of routes that visit either London Road
   # (149) or Craiglockhart (53). Run the query and notice the two services
   # that link these stops have a count of 2. Add a HAVING clause to restrict
   # the output to these two routes.
