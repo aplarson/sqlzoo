66c9bb67a3561e45a226a39b6c7fa07f95d5cb3c
Michelle Nosal <nosalcontact@yahoo.com>
2017-08-28 20:47:39 -0400

    01, 02, 03, 04

---
+++ README.md
@@ -0,0 +1,13 @@
+# SQL Zoo practice
+
+My answers to some SQL practice.
+
+To run the rspec tests, you can run `bundle install` from the
+"questions_and_answers" folder, and then run `./import_db.sh`
+from the "questions_and_answers/data" folder.
+
+Currently hung up on:
+```
+Some countries have populations more than three times that of any of their
+neighbors (in the same continent). Give the countries and continents.
+```
--- questions_and_answers/lib/01_select_basics.rb
+++ questions_and_answers/lib/01_select_basics.rb
@@ -23,12 +23,7 @@ end
 
 def select_population_of_germany
   execute(<<-SQL)
-    SELECT
-      population
-    FROM
-      countries
-    WHERE
-      name = 'Germany';
+    SELECT population FROM countries WHERE name = 'Germany';
   SQL
 end
 
@@ -36,12 +31,7 @@ def per_capita_gdp
   # Show the name and per capita gdp (gdp/population) for each country where
   # the area is over 5,000,000 km^2
   execute(<<-SQL)
-    SELECT
-      name, gdp/population AS gdp
-    FROM
-      countries
-    WHERE
-      area > 5000000;
+    SELECT name, (gdp/population) AS per_capita FROM countries WHERE area > 5000000;
   SQL
 end
 
@@ -49,12 +39,7 @@ def small_and_wealthy
   # Show the name and continent of countries where the area is less than 2,000
   # and the gdp is more than 5,000,000,000.
   execute(<<-SQL)
-    SELECT
-      name, continent
-    FROM
-      countries
-    WHERE
-      area < 2000 AND gdp > 5000000000;
+    SELECT name, continent FROM countries WHERE area < 2000 AND gdp > 5000000000;
   SQL
 end
 
@@ -62,24 +47,14 @@ def scandinavia
   # Show the name and the population for 'Denmark', 'Finland', 'Norway', and
   # 'Sweden'
   execute(<<-SQL)
-    SELECT
-      name, population
-    FROM
-      countries
-    WHERE
-      name IN ('Denmark', 'Finland', 'Norway', 'Sweden');
+    SELECT name, population FROM countries WHERE name IN ('Denmark', 'Finland', 'Norway', 'Sweden');
   SQL
 end
 
 def starts_with_g
   # Show each country that begins with the letter G
   execute(<<-SQL)
-    SELECT
-      name
-    FROM
-      countries
-    WHERE
-      name LIKE 'G%';
+    SELECT name FROM countries WHERE name LIKE 'G%'
   SQL
 end
 
@@ -88,11 +63,6 @@ def just_the_right_size
   # with an area between 200,000 and 250,000.
   # BETWEEN allows range checking - note that it is inclusive.
   execute(<<-SQL)
-    SELECT
-      name, area/1000
-    FROM
-      countries
-    WHERE
-      area BETWEEN 200000 AND 250000;
+    SELECT name, (area / 1000) AS k_sq_km FROM countries WHERE area BETWEEN 200000 AND 250000;
   SQL
 end
